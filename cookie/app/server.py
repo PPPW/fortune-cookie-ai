f82456a9d186bb0926d19cb451dedb82fdd0a956
PPPW <pei.wang.fudan@gmail.com>
2019-02-14 20:14:23 -0500

    split to requests

--- cookie/app/server.py
+++ cookie/app/server.py
@@ -7,12 +7,15 @@ from io import BytesIO
 import random
 
 from fastai import *
-from fastai.vision import *
+from fastai.text import *
 
 export_file_url = 'https://www.dropbox.com/s/vay47aricy4tdou/cookie_export.pkl?dl=1'
 export_file_name = 'cookie_export.pkl'
 
 starter = ['you', 'the', 'a', 'your', 'if', 'it', 'do', 'be', "don't", 'never', 'there', 'love', 'when', "it's", 'life', 'all', 'to', 'an', 'in', 'keep', 'everything', 'we', 'i', 'today', 'people', 'nothing', 'let', 'stop', 'for', 'good']
+puncs = '.,!?:;"' + "'"
+n_word = 3
+device = None
 path = Path(__file__).parent
 
 app = Starlette()
@@ -30,6 +33,8 @@ async def setup_learner():
     await download_file(export_file_url, path/'models'/export_file_name)
     try:
         learn = load_learner(path/'models', export_file_name)
+        global device
+        device = one_param(learn.model).device
         return learn
     except RuntimeError as e:
         if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
@@ -39,28 +44,34 @@ async def setup_learner():
         else:
             raise
 
+def predict(learn, text, n_words, reset, temperature=1., sep=' ', decoder=decode_spec_tokens):     
+    new_idx = []
+    if reset:
+        xb,yb = learn.data.one_item(text)
+        learn.model.reset()
+        new_idx = [learn.data.vocab.stoi[TK_MAJ]] + xb.tolist()[0][1:]
+    else:        
+        if text[-1] in puncs: text = text[-1]
+        xb = tensor([[learn.data.vocab.stoi[text]]]).to(device)
+        yb = tensor([0]).to(device)
+    i_BOS = learn.data.vocab.stoi[BOS]
+    is_end = False    
+    for _ in range(n_words):
+        res = learn.pred_batch(batch=(xb,yb))[0][-1]
+        res[learn.data.vocab.stoi[UNK]] = 0.        
+        if temperature != 1.: res.pow_(1 / temperature)
+        idx = torch.multinomial(res, 1).item()
+        if idx == i_BOS: 
+            is_end = True
+            break
+        new_idx.append(idx)
+        xb = xb.new_tensor([idx])[None]    
+    return sep.join(decoder(learn.data.vocab.textify(new_idx, sep=None))), is_end
+
 def format_output(sentence):    
-    try:
-        last_period = sentence.rindex('.')
-        sentence = sentence[:last_period+1]
-    except ValueError:
-        sentence += ' .'
-    words = sentence.split()
-    res = words[0].capitalize()
-    for i in range(1, len(words)):
-        if words[i] in ['xxmaj', 'xxup']: continue   
-        if words[i] == 'xxbos': break
-        if words[i] in '.,!?:;"' or words[i][0] == '\'' or words[i][:2] == 'n\'':            
-            res += words[i]
-            continue
-        res += ' '
-        if words[i-1] == 'xxmaj': 
-            res += words[i].capitalize()
-        elif words[i-1] == 'xxup':
-            res += words[i].upper()
-        else:
-            res += words[i]
-    return res
+    for punc in puncs:
+        sentence = sentence.replace(' ' + punc, punc)
+    return sentence
 
 
 loop = asyncio.get_event_loop()
@@ -75,8 +86,15 @@ def index(request):
 
 @app.route('/gen_sentence', methods=['POST'])
 async def gen_sentence(request):    
-    prediction = learn.predict(random.choice(starter) + ' ', 30, temperature=0.75)
-    return JSONResponse({'result': format_output(prediction)})
+    data = await request.form()
+    word = data['word']
+    reset = False
+    if word == '':
+        word = random.choice(starter)
+        reset = True
+    prediction, is_end = predict(learn, word, n_word, reset, temperature=0.75)
+    #await asyncio.sleep(1)
+    return JSONResponse({'result': format_output(prediction), 'end': is_end})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
--- cookie/app/static/app.js
+++ cookie/app/static/app.js
@@ -1,51 +1,55 @@
-let isOpen = false, $msg, $loader;
+let isOpen = false, $msg, $title, $cookie
+    loc = window.location,
+    url = `${loc.protocol}//${loc.hostname}:${loc.port}/gen_sentence`;
 
 $(function () {    
     $msg = $('#msg');
-    $loader = $('#loader');
-    $loader.hide();
-    let $cookie = $('#cookie'),
-        $title = $('.title');
-
+    $title = $('.title');
+    $cookie = $('#cookie');
+       
     $('body').click(function () {
         if (isOpen) {
             $cookie.attr('src', '../static/images/close.jpg');
             $title.html('Our AI has placed your fortune inside the cookie. <br />Click to open and view it!');            
-
+            $msg.find('p').text('');
             $msg.animate({
                 height: '0px',
                 opacity: 0
             }, 500);
         }
         else {
-            $cookie.attr('src', '../static/images/open.jpg');
-            $title.html('Want more? Click again to get another cookie! <br /> &nbsp');
+            $cookie.attr('src', '../static/images/spinner.gif');          
             
-            let loc = window.location;
-            let url = `${loc.protocol}//${loc.hostname}:${loc.port}/gen_sentence`;
-            $.ajax({
-                type: "POST",
-                url: url,          
-                beforeSend: function() {
-                    $loader.show();
-                },
-                complete: function(){
-                    $loader.hide();
-                },  
-                success: onSuccess,
-                dataType: "json"
-            });
+            $msg.animate({
+                height: '100px',
+                opacity: 1
+            }, 500);
+            query('');
         }
         isOpen = !isOpen;
     });
 })
 
 
-function onSuccess(data) {
-    let msg = data.result;
-    $msg.find('p').text(msg);
-    $msg.animate({
-        height: '100px',
-        opacity: 1
-    }, 500);
+function query(last) {
+    $.ajax({
+        type: 'POST',
+        url: url,   
+        data: { word: last },  
+        dataType: 'json',
+        success: function (data) {            
+            let end = data.end;
+            let current = $msg.find('p').text();
+            $msg.find('p').text(current + ' ' + data.result);            
+            let words = data.result.split(' ');
+            let last = words[words.length-1];
+            if (!end) {                
+                query(last);
+            }
+            else {
+                $title.html('Want more? Click again to get another cookie! <br /> &nbsp');
+                $cookie.attr('src', '../static/images/open.jpg');
+            }
+        },        
+    });
 }
